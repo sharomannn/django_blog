d41892cb13bf0ffa7824211d58565f798bfe46d0
sharomannn <pvp380@gmail.com>
2022-10-24 22:16:24 +0500

    blog tests

--- blog/functional_tests.py
+++ blog/functional_tests.py
@@ -27,6 +27,21 @@ class BasicInstallTest(unittest.TestCase):
         self.assertIn('Шарипов Роман',
                       header.text)
 
+    def test_home_page_blog(self):
+        # Под шапкой расположен блок со статьями
+        """Проверка наличия блога на главной странице"""
+        self.browser.get('http://127.0.0.1:8000/')
+        article_list = self.browser.find_element_by_class_name('article-list')
+        self.assertTrue(article_list)
+
+    def test_home_page_blog_articles(self):
+        # Под шапкой расположен блок со статьями
+        """Проверка наличия контента в блоге"""
+        self.browser.get('http://127.0.0.1:8000/')
+        article_title = self.browser.find_element_by_class_name('article-title')
+        article_summary = self.browser.find_element_by_class_name('article-summary')
+        self.assertTrue(article_title)
+        self.assertTrue(article_summary)
 
 if __name__ == '__main__':
     unittest.main()
