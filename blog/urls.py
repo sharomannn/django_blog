10188d8c74f8868c8e97057482e81565960d54b6
sharomannn <pvp380@gmail.com>
2022-10-27 16:20:38 +0500

    5 minutes before project submission

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 import blog.views
 
 urlpatterns = [
-    path('', blog.views.home_page)
+    path('', blog.views.home_page),
+    path('post/<int:post_id>', blog.views.post_page),
 ]
--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,9 @@ from blog import models
 def home_page(request):
     article = models.Article.objects.all
     return render(request, 'blog.html', context={'article_list':article})
+
+def post_page(request, post_id):
+    context = {
+        'post': models.Article.objects.get(id=post_id)
+    }
+    return render(request, "single.html", context)
